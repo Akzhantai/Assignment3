383b97096662b3d28e0588c4fa275a817d62820e
Akzhantai <musabekovaanara17@gmail.com>
2023-04-30 18:26:19 +0600

    MyLinkedListStack

--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -1,4 +1,6 @@
-import java.util.EmptyStackException;
-
 public class MyLinkedListStack<T> {
+    private MyLinkedList<T> stack;
+    public MyLinkedListStack() {
+        stack = new MyLinkedList<>();
+    }
 }
