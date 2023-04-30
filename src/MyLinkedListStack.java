af99687e938e00dd0cf372e3dd7a0aa5847a5a67
Akzhantai <musabekovaanara17@gmail.com>
2023-04-30 19:28:01 +0600

    MyLinkedListStack

--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -1,6 +1,11 @@
 public class MyLinkedListStack<T> {
     private MyLinkedList<T> stack;
+
     public MyLinkedListStack() {
         stack = new MyLinkedList<>();
+    }
+
+    public void push(T element) {
+        stack.addFirst(element);
     }
 }
