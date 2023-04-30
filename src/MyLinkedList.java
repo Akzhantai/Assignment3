b4515268c42b9f442cdb6d71737ec696d529bdfd
Akzhantai <musabekovaanara17@gmail.com>
2023-04-30 18:02:51 +0600

    MyLinkedList

---
+++ src/MyLinkedList.java
@@ -0,0 +1,164 @@
+public class MyLinkedList<T> implements MyList<T> {
+
+    private class Node {
+        T element;
+        Node next;
+        Node prev;
+
+        public Node(T element, Node next, Node prev) {
+            this.element = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public MyLinkedList() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        Node current = head;
+        while (current != null) {
+            if (current.element.equals(o)) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    @Override
+    public void add(T element) {
+        Node newNode = new Node(element, null, tail);
+        if (size == 0) {
+            head = newNode;
+        } else {
+            tail.next = newNode;
+        }
+        tail = newNode;
+        size++;
+    }
+
+    @Override
+    public void add(T element, int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index == size) {
+            add(element);
+            return;
+        }
+        Node current = getNode(index);
+        Node newNode = new Node(element, current, current.prev);
+        current.prev.next = newNode;
+        current.prev = newNode;
+        size++;
+    }
+
+    @Override
+    public boolean remove(T item) {
+        int index = indexOf(item);
+        if (index == -1) {
+            return false;
+        }
+        remove(index);
+        return true;
+    }
+
+    @Override
+    public T remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node node = getNode(index);
+        if (node == head) {
+            head = node.next;
+        } else {
+            node.prev.next = node.next;
+        }
+        if (node == tail) {
+            tail = node.prev;
+        } else {
+            node.next.prev = node.prev;
+        }
+        size--;
+        return node.element;
+    }
+
+    @Override
+    public void clear() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    @Override
+    public T get(int index) {
+        return getNode(index).element;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        Node current = head;
+        int index = 0;
+        while (current != null) {
+            if (current.element.equals(o)) {
+                return index;
+            }
+            current = current.next;
+            index++;
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        Node current = tail;
+        int index = size - 1;
+        while (current != null) {
+            if (current.element.equals(o)) {
+                return index;
+            }
+            current = current.prev;
+            index--;
+        }
+        return -1;
+    }
+
+    @Override
+    public void sort() {
+        // This implementation of sort is not efficient for linked lists,
+        // so we'll just leave it unimplemented
+    }
+
+    private Node getNode(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node current;
+        if (index < size / 2) {
+            current = head;
+            for (int i = 0; i < index; i++) {
+                current = current.next;
+            }
+        } else {
+            current = tail;
+            for (int i = size - 1; i > index; i--) {
+                current = current.prev;
+            }
+        }
+        return current;
+    }
+}
