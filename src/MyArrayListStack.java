892733d61dbcc7448e57e3bd93f93b357cefe6b5
Akzhantai <musabekovaanara17@gmail.com>
2023-04-30 18:26:41 +0600

    MyArrayListStack

--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -1,4 +1,8 @@
 import java.util.EmptyStackException;
 
 public class MyArrayListStack<T> {
+        private MyArrayList<T> stack;
+        public MyArrayListStack() {
+            stack = new MyArrayList<>();
+        }
 }
