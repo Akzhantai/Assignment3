ffdcf7c4db556887c7b580c32c41058e98c11395
Akzhantai <musabekovaanara17@gmail.com>
2023-05-01 15:04:05 +0600

    MyArrayListQueue and MyLinkedListQueue

--- src/MyArrayListQueue.java
+++ src/MyArrayListQueue.java
@@ -1,4 +1,31 @@
 import java.util.NoSuchElementException;
 public class MyArrayListQueue<T>{
     private MyArrayList<T> queue;
+    public MyArrayListQueue() {
+        queue = new MyArrayList<>();
+    }
+    public void enqueue(T element) {
+        queue.addLast(element);
+    }public T dequeue() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return queue.removeFirst();
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return queue.getFirst();
+    }
+
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
+
+    public int size() {
+        return queue.size();
+    }
+
 }
